836a07ab5d055ff3d9a3af4641d60f2ff6746cc6
ssstrz <codecats@gmail.com>
2014-05-09 21:45:18 +0200

    bundle for image filtering added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new CodeCats\PanelBundle\CodeCatsPanelBundle(),
             new CodeCats\FrontBundle\CodeCatsFrontBundle(),
             new CodeCats\TestBundle\CodeCatsTestBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
