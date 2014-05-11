b73e4b931d1e7eb953bbc023faea79d19c22b211
ssstrz <codecats@gmail.com>
2014-05-11 17:55:20 +0200

    new view

--- src/CodeCats/FrontBundle/Controller/DefaultController.php
+++ src/CodeCats/FrontBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace CodeCats\FrontBundle\Controller;
 
+use CodeCats\PanelBundle\Entity\Progress;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -11,10 +12,13 @@ class DefaultController extends Controller
         $em         = $this->getDoctrine()->getManager();
         $user       = $em->getRepository('CodeCatsPanelBundle:User');
         $category   = $em->getRepository('CodeCatsPanelBundle:Category');
+        $progress   = $em->getRepository('CodeCatsPanelBundle:Progress');
+
 
         return $this->render('CodeCatsFrontBundle:Default:index.html.twig', array(
             'users'         => $user->getMostActive(3),
-            'categories'    => $category->findAll()
+            'categories'    => $category->findAll(),
+            'progresses'    => $progress->findBy(array(), array('ended' => 'DESC'), 5)
         ));
     }
 }
